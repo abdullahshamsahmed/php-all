af4039835fd5a02490d180e8db1ce312b4484940
Tarif <t@gmail.com>
2019-08-23 00:54:47 +0600

    checkout datatype

---
+++ 2.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+</head>
+<body>
+	<?php
+		// to know the types and value of a variable use this function :
+		$varibaleName=15;
+		var_dump ($varibaleName);
+		
+	?>
+</body>
+</html>
