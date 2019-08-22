ae9b17919e4fae588d532ab403d0c6491e9fe64c
Tarif <t@gmail.com>
2019-08-23 01:30:39 +0600

    constant declearation

---
+++ 4.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+</head>
+<body>
+	<?php
+		// about constant :
+		# constant varibale ar age $ bose na. letter or _ dia start hoy. always global variable ar moton kaj kore
+		define ("caseSensitive_Nospace_variableName","msg");
+		echo caseSensitive_Nospace_variableName;
+	?>
+</body>
+</html>
