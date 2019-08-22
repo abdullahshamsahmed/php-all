68feb17027729b2e14cd6f4a0f4b308d3249e0f0
Tarif <t@gmail.com>
2019-08-23 01:17:51 +0600

    word position in string

--- 3.php
+++ 3.php
@@ -10,7 +10,7 @@
 		echo strlen("ai line koyta index ase, 1 theke hisab suru kore");
 		echo str_word_count("ai line koyta word ase");
 		echo strrev("ai line ulta kore dkhabe,pottek ta letter position o");
-		
+		echo strpos("aita name string position word ar position ar index ber kore akdom surute j khane pay tar index value,jmn ai khane a word ar postion ber korbo","word");
 	?>
 </body>
 </html>
