ec460562d7c15f8c4759de5bc3029493c9577a53
Tarif <t@gmail.com>
2019-08-23 01:20:39 +0600

    word replace

--- 3.php
+++ 3.php
@@ -11,6 +11,7 @@
 		echo str_word_count("ai line koyta word ase");
 		echo strrev("ai line ulta kore dkhabe,pottek ta letter position o");
 		echo strpos("aita name string position word ar position ar index ber kore akdom surute j khane pay tar index value,jmn ai khane a word ar postion ber korbo","word");
+		echo str_replace("old word","added new word","old word sentens");
 	?>
 </body>
 </html>
