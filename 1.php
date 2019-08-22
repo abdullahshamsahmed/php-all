dd21882227090cf10b91a96c121e741d52a0d170
Tarif <t@gmail.com>
2019-08-23 00:44:17 +0600

    comments styles

--- 1.php
+++ 1.php
@@ -5,6 +5,6 @@
 	<title></title>
 </head>
 <body>
-	<?php		echo "PRINT COMMAND"	?>
+	<?php		echo "PRINT COMMAND"		//comment styles		#comment2		/*comment 3	      comment 3		*/			?>
 </body>
 </html>
