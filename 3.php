ab6c8e9b16be5dcfc8b17d087a818a70cf5c7a2d
Tarif <t@gmail.com>
2019-08-23 01:01:43 +0600

    letter number

---
+++ 3.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+</head>
+<body>
+	<?php
+		// all about string functions :
+		echo strlen("ai line koyta index ase, 1 theke hisab suru kore");
+		
+	?>
+</body>
+</html>
